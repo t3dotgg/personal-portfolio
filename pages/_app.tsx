24ce2389efb53dac4d355289000931da487f8e68
tigerabrodi <tigerabrodi@gmail.com>
2021-08-21 15:00:06 +0200

    chore: change name of global styles file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import "../theme/globals.css";
+import "../theme/globalStyles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
