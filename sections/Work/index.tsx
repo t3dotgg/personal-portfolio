0fddb1e34f3550292a7781fbd8a82f8ab2f5fb7e
tigerabrodi <tigerabrodi@gmail.com>
2021-09-02 06:45:43 +0200

    refactor(Work): extract projects data and map them out in the section

--- sections/Work/index.tsx
+++ sections/Work/index.tsx
@@ -1,7 +1,4 @@
 import Link from "next/link";
-import madaraDemo from "../../assets/madara.gif";
-import ravenyDemo from "../../assets/raveny.gif";
-import konohaDemo from "../../assets/konoha.gif";
 import {
   WorkDescription,
   WorkList,
@@ -17,6 +14,7 @@ import {
   External,
 } from "./styles";
 import { useOnScreen } from "../../hooks/useOnScreen";
+import { projects } from "./projects";
 
 export const Work = () => {
   const { isVisible, setIntersectingElement } = useOnScreen();
@@ -29,73 +27,37 @@ export const Work = () => {
         descriptions and links.
       </WorkDescription>
       <WorkList>
-        <WorkItem>
-          <ItemTitle>Madara</ItemTitle>
-          <ItemDescription>
-            A way for people to manage their tasks.
-          </ItemDescription>
-          <ItemDemo
-            alt="Demo showcasing the project, what it does and the problem it solves for people."
-            src={madaraDemo.src}
-          />
-          <Link href="https://github.com/tigerabrodi/Madara" passHref>
-            <ItemCodeLink target="_blank" rel="noopener noreferrer">
-              <Github aria-hidden="true" />
-              Code
-            </ItemCodeLink>
-          </Link>
-          <Link href="https://tigerabrodi-madara.netlify.app/" passHref>
-            <ItemLiveLink target="_blank" rel="noopener noreferrer">
-              <External aria-hidden="true" />
-              Live
-            </ItemLiveLink>
-          </Link>
-        </WorkItem>
-        <WorkItem>
-          <ItemTitle>Raveny</ItemTitle>
-          <ItemDescription>
-            A website where people can find recipes and find them based on
-            different criteria.
-          </ItemDescription>
-          <ItemDemo
-            alt="Demo showcasing the project, what it does and the problem it solves for people."
-            src={ravenyDemo.src}
-          />
-          <Link href="https://github.com/tigerabrodi/Raveny" passHref>
-            <ItemCodeLink target="_blank" rel="noopener noreferrer">
-              <Github aria-hidden="true" />
-              Code
-            </ItemCodeLink>
-          </Link>
-          <Link href="https://raveny.netlify.app/" passHref>
-            <ItemLiveLink target="_blank" rel="noopener noreferrer">
-              <External aria-hidden="true" />
-              Live
-            </ItemLiveLink>
-          </Link>
-        </WorkItem>
-        <WorkItem>
-          <ItemTitle>Konoha</ItemTitle>
-          <ItemDescription>
-            A simple way to search for Anime/Manga characters.
-          </ItemDescription>
-          <ItemDemo
-            alt="Demo showcasing the project, what it does and the problem it solves for people."
-            src={konohaDemo.src}
-          />
-          <Link href="https://github.com/tigerabrodi/konoha" passHref>
-            <ItemCodeLink target="_blank" rel="noopener noreferrer">
-              <Github aria-hidden="true" />
-              Code
-            </ItemCodeLink>
-          </Link>
-          <Link href="https://tigerabrodi-konoha.netlify.app/" passHref>
-            <ItemLiveLink target="_blank" rel="noopener noreferrer">
-              <External aria-hidden="true" />
-              Live
-            </ItemLiveLink>
-          </Link>
-        </WorkItem>
+        {projects.map(
+          ({
+            title,
+            description,
+            demoSource,
+            sourceCodeLink,
+            liveVersionLink,
+            id,
+          }) => (
+            <WorkItem key={id}>
+              <ItemTitle>{title}</ItemTitle>
+              <ItemDescription>{description}</ItemDescription>
+              <ItemDemo
+                alt="Demo showcasing the project, what it does and the problem it solves for people."
+                src={demoSource}
+              />
+              <Link href={sourceCodeLink} passHref>
+                <ItemCodeLink target="_blank" rel="noopener noreferrer">
+                  <Github aria-hidden="true" />
+                  Code
+                </ItemCodeLink>
+              </Link>
+              <Link href={liveVersionLink} passHref>
+                <ItemLiveLink target="_blank" rel="noopener noreferrer">
+                  <External aria-hidden="true" />
+                  Live
+                </ItemLiveLink>
+              </Link>
+            </WorkItem>
+          )
+        )}
       </WorkList>
     </WorkSection>
   );
---
+++ sections/Work/projects.ts
@@ -0,0 +1,31 @@
+import madaraDemo from "../../assets/madara.gif";
+import ravenyDemo from "../../assets/raveny.gif";
+import konohaDemo from "../../assets/konoha.gif";
+
+export const projects = [
+  {
+    title: "Madara",
+    description: "A way for people to manage their tasks.",
+    demoSource: madaraDemo.src,
+    sourceCodeLink: "https://github.com/tigerabrodi/Madara",
+    liveVersionLink: "https://tigerabrodi-madara.netlify.app/",
+    id: "1",
+  },
+  {
+    title: "Raveny",
+    description:
+      "A website where people can find recipes and find them based on different criteria.",
+    demoSource: ravenyDemo.src,
+    sourceCodeLink: "https://github.com/tigerabrodi/Raveny",
+    liveVersionLink: "https://raveny.netlify.app/",
+    id: "2",
+  },
+  {
+    title: "Konoha",
+    description: "A simple way to search for Anime/Manga characters.",
+    demoSource: konohaDemo.src,
+    sourceCodeLink: "https://github.com/tigerabrodi/konoha",
+    liveVersionLink: "https://tigerabrodi-konoha.netlify.app/",
+    id: "3",
+  },
+];
