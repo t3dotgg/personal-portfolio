91ff10cd5cc15f052aec5ef556db7f4ce7a6717c
tigerabrodi <tigerabrodi@gmail.com>
2021-09-08 18:05:13 +0200

    feat(Contact): fix text spacing

--- sections/Contact/index.tsx
+++ sections/Contact/index.tsx
@@ -17,12 +17,12 @@ export const Contact = () => {
         Contact.
       </ContactTitle>
       <ContactDescription>
-        The fastest way to get in touch with me is by sending me a message on
+        The fastest way to get in touch with me is by sending me a message on{" "}
         <Link href="https://twitter.com/TAbrodi" passHref>
           <DescriptionLink target="_blank" rel="noopener noreferrer">
             Twitter.
           </DescriptionLink>
-        </Link>
+        </Link>{" "}
         You can also shoot me an email if you want, whether it is discussing an
         opportunity or asking a question, I will try my best to get back to you.
       </ContactDescription>
