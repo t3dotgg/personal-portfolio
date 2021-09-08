a269d6b4370fdfeb3f779f2b6660a279d847ca79
tigerabrodi <tigerabrodi@gmail.com>
2021-09-08 18:04:55 +0200

    fix: image preview link

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ import { Footer } from "../sections/Footer";
 import { Home } from "../sections/Home";
 import { Navigation } from "../sections/Navigation";
 import { Work } from "../sections/Work";
-import FaceImage from "../../assets/avatar.jpeg";
 
 const title = "Tiger Abrodi's Portfolio";
 const description =
@@ -25,11 +24,11 @@ const Page: NextPage = () => {
         <meta name="twitter:site" content="@TAbrodi" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={FaceImage.src} />
+        <meta name="twitter:image" content="/assets/avatar.jpeg" />
 
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={FaceImage.src} />
+        <meta property="og:image" content="/assets/avatar.jpeg" />
       </Head>
       <Navigation mainRef={mainRef} />
       <main id="main" ref={mainRef} tabIndex={-1}>
