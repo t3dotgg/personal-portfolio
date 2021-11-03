76caa2c29157fae33e24180d81c31024e985b981
tigerabrodi <tigerabrodi@gmail.com>
2021-11-03 06:10:48 +0100

    feat: use rem for breakpoints due to accessibility concerns

--- theme/media.ts
+++ theme/media.ts
@@ -1,5 +1,5 @@
 const customMediaQuery = (maxWidth: number) =>
-  `@media (min-width: ${maxWidth}px)`;
+  `@media (min-width: ${maxWidth / 16}rem)`;
 
 export const media = {
   custom: customMediaQuery,
