627f0b7eb3747d6aeb1d779d78cb8066a859a9cf
tigerabrodi <tigerabrodi@gmail.com>
2021-11-03 06:06:11 +0100

    feat(About): major adjustmens to layout on mobile

--- sections/About/styles.ts
+++ sections/About/styles.ts
@@ -4,31 +4,16 @@ import { media } from "../../theme/media";
 import { theme } from "../../theme/theme";
 
 export const AboutSection = styled.section<{ shouldFadeIn: boolean }>`
-  display: grid;
   width: 100%;
-  align-items: flex-start;
-  justify-items: center;
-  align-content: space-between;
-  grid-template-areas:
-    "image"
-    "title"
-    "text"
-    "technologies"
-    ".";
-  grid-template-rows: 12rem 5rem 29rem 40rem 8rem;
   margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 80px;
+  height: 93rem;
   ${media.tablet} {
     margin-top: 0;
-    align-items: center;
-    grid-template-areas:
-      "title image"
-      "text image"
-      "text technologies";
-    grid-template-rows: 11rem 14rem 57rem;
     width: 85%;
-  }
-  ${media.desktop} {
-    grid-template-rows: 14rem 19rem 57rem;
   }
   transition: transform 1s, opacity 2s;
   opacity: 0;
@@ -42,7 +27,6 @@ export const AboutSection = styled.section<{ shouldFadeIn: boolean }>`
 `;
 
 export const AboutImage = styled.img`
-  grid-area: image;
   height: 100%;
   border-radius: 50%;
   box-shadow: 0 0.2rem 0.2rem black;
@@ -62,11 +46,11 @@ export const AboutImage = styled.img`
 `;
 
 export const AboutTitle = styled.h1`
-  grid-area: title;
   font-family: ${theme.Oxanium};
   color: ${theme.Pink};
   font-weight: 600;
   font-size: 3rem;
+  margin-top: 10px;
   ${media.tablet} {
     font-size: 6rem;
     align-self: flex-end;
@@ -77,7 +61,6 @@ export const AboutTitle = styled.h1`
 `;
 
 export const AboutTextWrapper = styled.p`
-  grid-area: text;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -87,8 +70,11 @@ export const AboutTextWrapper = styled.p`
   font-weight: 400;
   font-size: 1.4rem;
   text-align: center;
-  width: 25ch;
   height: 100%;
+  margin-top: 15px;
+  width: 26ch;
+  justify-content: flex-start;
+  row-gap: 25px;
   ${media.tablet} {
     font-size: 2rem;
     align-self: flex-start;
@@ -108,12 +94,11 @@ export const AboutText = styled.span`
 `;
 
 export const TechnologiesWrapper = styled.div`
-  grid-area: technologies;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
-  height: 100%;
+  margin-top: 40px;
   ${media.tablet} {
     margin-top: 70px;
     width: 100%;
@@ -122,11 +107,12 @@ export const TechnologiesWrapper = styled.div`
 
 export const TechnologiesText = styled.p`
   font-family: ${theme.Montserrat};
-  font-size: 1.4rem;
   font-weight: 600;
   width: 26ch;
   text-align: center;
   color: ${theme.Orange};
+  line-height: 1.5;
+  font-size: 1.5rem;
   ${media.tablet} {
     font-size: 2.2rem;
     font-family: ${theme.Oxanium};
@@ -138,13 +124,13 @@ export const TechnologiesText = styled.p`
 `;
 
 export const TechnologiesContainer = styled.div`
-  height: 80%;
   display: flex;
   align-items: center;
   justify-content: space-evenly;
-  width: 23rem;
   position: relative;
-  left: 1.5rem;
+  width: 27rem;
+  left: 2.2rem;
+  height: 90%;
   ${media.tablet} {
     left: 0;
     width: 100%;
@@ -159,6 +145,8 @@ export const TechnologiesList = styled.ul`
   align-items: flex-start;
   justify-content: space-evenly;
   flex-direction: column;
+  row-gap: 20px;
+  margin-top: 50px;
   ${media.tablet} {
     align-items: center;
   }
@@ -181,11 +169,11 @@ export const RightArrow = styled(RightArrowSVG)`
 `;
 
 export const TechnologyText = styled.span`
-  font-size: 1rem;
   font-weight: 500;
   font-family: ${theme.Oxanium};
   color: ${theme.Orange};
   padding-left: 5px;
+  font-size: 1.2rem;
   ${media.tablet} {
     font-size: 1.7rem;
     padding-left: 10px;
